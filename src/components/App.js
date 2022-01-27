44c851e8fc7ce99becafc714ecf47d00234d627a
shevchenko-andrey <andreases_mobimoz@ukr.net>
2022-01-28 00:35:36 +0200

    refactor app

--- src/components/App.js
+++ src/components/App.js
@@ -1,99 +1,77 @@
-import React, { Component } from 'react';
-
 import toast, { Toaster } from 'react-hot-toast';
-
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import { GlobalStyle } from '../Base/BaseStyle';
 
+import useLocalStorage from '../Cookies/useLocalStorage';
 import ContactForm from './Form';
 import Section from './Section';
 import Contacts from './Contacts';
 import Filter from './Filter';
 
 const STORAGE_CONTACTS_KEY = 'contacts';
-class App extends Component {
-  static defaultProps = {
-    state: {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
-    },
-  };
 
-  state = this.props.state;
+const App = () => {
+  const [contacts, setContacts] = useLocalStorage(STORAGE_CONTACTS_KEY, [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
-  componentDidMount() {
-    const contacts = localStorage.getItem(STORAGE_CONTACTS_KEY);
-    if (contacts) {
-      this.setState({ contacts: JSON.parse(contacts) });
-    }
-  }
-  componentDidUpdate(prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem(STORAGE_CONTACTS_KEY, JSON.stringify(contacts));
-    }
-  }
-  handleSubmitForm = ({ name, number }) => {
-    const { contacts } = this.state;
+  const [filter, setFilter] = useState('');
+
+  const handleSubmitForm = ({ name, number }) => {
     const isDubled = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
-    this.setState(({ contacts }) => {
-      if (isDubled) {
-        toast.error(`${name} is already in contacts`);
-        return;
-      }
-      return {
-        contacts: [
-          ...contacts,
-          {
-            id: nanoid(),
-            name: name,
-            number: number,
-          },
-        ],
-      };
-    });
+    if (isDubled) {
+      toast.error(`${name} is already in contacts`);
+      return;
+    }
+
+    setContacts([
+      ...contacts,
+      {
+        id: nanoid(),
+        name: name,
+        number: number,
+      },
+    ]);
   };
-  handleDeleteContact = contactId => {
-    const { contacts } = this.state;
-    this.setState({
-      contacts: contacts.filter(contact => contactId !== contact.id),
-    });
+
+  const handleDeleteContact = contactId => {
+    const newContacts = contacts.filter(contact => contactId !== contact.id);
+    setContacts(newContacts);
   };
-  changeFilter = e => {
+
+  const changeFilter = e => {
     const { value } = e.currentTarget;
-    this.setState({ filter: value });
+    setFilter(value);
   };
-  render() {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    const visibleContacts = contacts.filter(({ name }) => {
-      return name.toLowerCase().includes(normalizedFilter);
-    });
-    return (
-      <>
-        <GlobalStyle />
-        <Section title="PhoneBook">
-          <ContactForm onSubmit={this.handleSubmitForm} />
-        </Section>
-        <Section title="Contacts">
-          <Filter value={filter} onChange={this.changeFilter} />
-          <Contacts
-            contacts={visibleContacts}
-            onDeleteContact={this.handleDeleteContact}
-          />
-        </Section>
-        <Toaster />
-      </>
-    );
-  }
-}
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(({ name }) => {
+    return name.toLowerCase().includes(normalizedFilter);
+  });
+
+  return (
+    <>
+      <GlobalStyle />
+      <Section title="PhoneBook">
+        <ContactForm onSubmit={handleSubmitForm} />
+      </Section>
+      <Section title="Contacts">
+        <Filter value={filter} onChange={changeFilter} />
+        <Contacts
+          contacts={visibleContacts}
+          onDeleteContact={handleDeleteContact}
+        />
+      </Section>
+      <Toaster />
+    </>
+  );
+};
 
 export default App;
